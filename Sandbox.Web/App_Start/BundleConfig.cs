194f6ba0d74b0b0b1f1eba4ba69eb9224e1a027d
mattbettinson <mjbettinson@gmail.com>
2014-01-16 20:35:58 +0000

    Adding search box and basic 'typeahead' functionality.

--- Sandbox.Web/App_Start/BundleConfig.cs
+++ Sandbox.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,12 @@ namespace Sandbox.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/typeahead").Include(
+                        "~/Scripts/typeahead.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/sandbox").Include(
+                        "~/Scripts/sandbox.js"));
+            
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
