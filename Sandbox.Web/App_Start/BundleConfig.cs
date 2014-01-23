67b69bad47afdf395edbde8fecbbdd2697578c95
mattbettinson <mjbettinson@gmail.com>
2014-01-23 21:45:57 +0000

    Adding SASS version of Bootstrap 3.

--- Sandbox.Web/App_Start/BundleConfig.cs
+++ Sandbox.Web/App_Start/BundleConfig.cs
@@ -27,7 +27,7 @@ namespace Sandbox.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/sass-bootstrap.css",
                       "~/Content/site.css"));
         }
     }
